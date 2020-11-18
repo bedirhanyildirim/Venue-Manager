9ee90154e4a095d920f5cfdb16d4e26b136ecfe2
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2020-11-18 18:57:52 +0100

    footer update

--- src/themes/footer.theme.vue
+++ src/themes/footer.theme.vue
@@ -1,47 +1,40 @@
 <template>
-    <div id="footer">
-        <div class="container">
-            <h5>Developed by <a href="http://bedirhanyildirim.com" target="_blank">Bedirhan Yıldırım</a></h5>
-        </div>
-    </div>
+  <div id="footer">
+    <container display="flex" align-items="center" justify-content="center">
+      <h5>Developed by <a href="http://bedirhanyildirim.com" target="_blank">Bedirhan Yıldırım</a></h5>
+    </container>
+  </div>
 </template>
 
 <script>
+import Container from "@/components/container";
 export default {
-    name: 'footer.theme.vue'
+  name: 'footer.theme.vue',
+  components: {Container}
 }
 </script>
 
 <style lang="scss" scoped>
 #footer {
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    position: fixed;
-    align-items: center;
-    justify-content: center;
-    background-color: #ffffff;
-    border-top: 1px solid #e5e5e5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffffff;
+
+  -webkit-box-shadow: 0px -1px 12px 0px rgba(0,0,0,0.1);
+  -moz-box-shadow: 0px -1px 12px 0px rgba(0,0,0,0.1);
+  box-shadow: 0px -1px 12px 0px rgba(0,0,0,0.1);
 }
 
 #footer {
-    .container {
-        width: 1000px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .container {
-        h5 {
-            padding: 8px 0;
-            font-size: 14px;
-            font-weight: normal;
-        }
-        a {
-            color: #004680;
-            font-weight: bold;
-        }
-    }
+  h5 {
+    padding: 8px 0;
+    font-size: 14px;
+    font-weight: normal;
+  }
+  a {
+    color: #004680;
+    font-weight: bold;
+  }
 }
 </style>
