38f022e386056aa2fc48d0e2d728f13dc8f7ea8a
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2020-11-24 12:40:51 +0100

    complete profile page - new design

--- src/components/complete.completeProfile.vue
+++ src/components/complete.completeProfile.vue
@@ -1,30 +1,29 @@
 <template>
 <div id="complete">
-    <div class="row">
-        <label for="username" class="input-name">Kullanıcı Adı:</label>
-        <input id="username" type="text" v-model="username" name="username" disabled/>
-    </div>
-    <div class="row">
-        <label for="email" class="input-name">Email:</label>
-        <input id="email" type="text" v-model="email" name="email" disabled/>
-    </div>
-    <div class="row">
-        <label for="name" class="input-name">Ad:</label>
-        <input id="name" type="text" v-model="name" name="name"/>
-    </div>
-    <div class="row">
-        <label for="surname" class="input-name">Soyad:</label>
-        <input id="surname" type="text" v-model="surname" name="surname"/>
-    </div>
-    <div class="row">
-        <label for="mobile" class="input-name">Telefon No:</label>
-        <input id="mobile" type="text" v-model="mobile" name="mobile"/>
-    </div>
-    <div class="row" style="justify-content: center; margin-bottom: 0;">
-        <div class="button">
-            <button @click="fill">Kaydet</button>
-        </div>
-    </div>
+  <div class="row">
+    <label for="username" class="input-name">Username</label>
+    <input id="username" type="text" v-model="username" name="username" disabled/>
+  </div>
+  <div class="row">
+    <label for="email" class="input-name">Email Address</label>
+    <input id="email" type="text" v-model="email" name="email" disabled/>
+  </div>
+  <div class="row">
+    <label for="name" class="input-name">Name</label>
+    <input id="name" type="text" v-model="name" name="name"/>
+  </div>
+  <div class="row">
+    <label for="surname" class="input-name">Surname</label>
+    <input id="surname" type="text" v-model="surname" name="surname"/>
+  </div>
+  <div class="row">
+    <label for="mobile" class="input-name">Phone Number</label>
+    <!--input id="mobile" type="tel" v-model="mobile" name="mobile"/-->
+    <the-mask id="mobile" v-model="mobile" name="mobile" mask="0### ### ## ##" value="" type="tel" masked placeholder="05XX XXX XX XX"></the-mask>
+  </div>
+  <div class="row" style="justify-content: center; margin-bottom: 0;">
+      <button class="button" @click="fill">Save</button>
+  </div>
 </div>
 </template>
 
@@ -32,122 +31,185 @@
 import store from '../store'
 import router from '../router'
 import { mapGetters } from 'vuex'
+import {TheMask} from 'vue-the-mask'
 import { usersCollection } from '../firebase/index'
 export default {
-    name: "complete.completeProfile",
-    store,
-    router,
-    data: function () {
-        return {
-            name: '',
-            surname: '',
-            mobile: '',
-            email: '',
-            username: ''
-        }
-    },
-    computed: {
-        ...mapGetters([
-            'getUser',
-            'loggedIn'
-        ])
-    },
-    created () {
-        this.email = this.getUser.email
-        this.username = this.email.split('@')[0]
-    },
-    methods: {
-        fill: function () {
-            usersCollection.doc(this.getUser.uid).set({
-                uid: this.getUser.uid,
-                email: this.getUser.email,
-                username: this.username,
-                name: this.name,
-                surname: this.surname,
-                mobile: this.mobile
-            }).then(res => {
-                console.log("Başarılı: " + res)
-                this.$store.dispatch('setUserInfo', {
-                    uid: this.getUser.uid,
-                    email: this.getUser.email,
-                    username: this.username,
-                    name: this.name,
-                    surname: this.surname,
-                    mobile: this.mobile
-                })
-                router.push("/")
-            }).catch(function (error) {
-                console.log(error)
-            })
-        }
+  name: "complete.completeProfile",
+  store,
+  router,
+  components: { TheMask },
+  data: function () {
+    return {
+      name: '',
+      surname: '',
+      mobile: '',
+      email: '',
+      username: ''
     }
+  },
+  computed: {
+    ...mapGetters([
+      'getUser',
+      'loggedIn',
+      'getUserInfo'
+    ])
+  },
+  created () {
+    this.email = this.getUser.email
+    this.username = this.email.split('@')[0]
+  },
+  methods: {
+    fill: function () {
+      usersCollection.doc(this.getUser.uid).set({
+        uid: this.getUser.uid,
+        email: this.getUser.email,
+        username: this.username,
+        name: this.name,
+        surname: this.surname,
+        mobile: this.mobile
+      }).then(res => {
+        console.log("Başarılı: " + res)
+        this.$store.dispatch('setUserInfo', {
+          uid: this.getUser.uid,
+          email: this.getUser.email,
+          username: this.username,
+          name: this.name,
+          surname: this.surname,
+          mobile: this.mobile
+        })
+        router.push("/")
+      }).catch(function (error) {
+        console.log(error)
+      })
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 #complete {
-    padding: 30px;
-    border-radius: 5px;
-    animation: fadeIn 1s;
-    background-color: #fafafa;
-    border: 1px solid #e5e5e5;
-    -webkit-animation: fadeIn 1s;
-}
-#complete {
-    .row {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        margin-bottom: 20px;
-    }
+  .row {
+    display: flex;
+    margin-bottom: 20px;
+    align-items: baseline;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .row {
     .input-name {
-        color: #707070;
-        font-size: 16px;
-        margin-right: 10px;
-        font-weight: normal;
-        display: inline-block;
+      color: #000000;
+      display: block;
+      font-size: 16px;
+      font-weight: 700;
+      margin: 14px 0 4px;
     }
     input {
-        width: 250px;
-        font-size: 14px;
-        padding: 10px 6px;
-        border-radius: 5px;
-        display: inline-block;
-        border: 1px solid #e5e5e5;
+      width: 100%;
+      height: 40px;
+      outline: none;
+      color: #000000;
+      font-size: 14px;
+      min-width: 400px;
+      appearance: none;
+      font-weight: 400;
+      line-height: 24px;
+      padding: 10px 16px;
+      border-radius: 8px;
+      display: inline-block;
+      box-sizing: border-box;
+      background-color: #f3f3f4;
+      transition: all 200ms ease;
+      border: 1px solid transparent;
+    }
+    input:hover {
+      background-color: #ffffff;
+      border-color: rgba(0,0,0,0.1);
+      box-shadow: 0 0 0 4px rgba(0,18,255,0.1);
     }
     input:focus {
-        box-shadow: 0px 0px 3px 0px rgba(0,70,128,.75);
+      background-color: #ffffff;
+      border-color: rgba(39,46,138,0.4);
+      box-shadow: 0 0 0 4px rgba(0,18,255,0.1);
+    }
+    input:disabled {
+      cursor: not-allowed;
+      background-color: #f7f7f7;
+    }
+    input:disabled:hover {
+      box-shadow: none;
+      border-color: #f7f7f7;
+      background-color: #f7f7f7;
+    }
+    input:disabled:focus {
+      border: none!important;
+      box-shadow: none!important;
+      background-color: #f7f7f7!important;
     }
     .button {
-        display: flex;
-        align-items: center;
-        justify-content: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    button {
+      height: 40px;
+      border: none;
+      outline: none;
+      color: #ffffff;
+      display: block;
+      cursor: pointer;
+      font-size: 14px;
+      max-width: 200px;
+      box-shadow: none;
+      appearance: none;
+      line-height: 20px;
+      position: relative;
+      padding: 10px 16px;
+      text-align: center;
+      border-radius: 8px;
+      font-weight: normal;
+      text-decoration: none;
+      box-sizing: border-box;
+      background-color: #272E8A;
+      transition: all 200ms ease;
+    }
+    button:hover:enabled {
+      cursor: pointer;
+      background-color: #2e39c4;
     }
     .button {
-        button {
-            width: 100%;
-            color: #ffffff;
-            display: block;
-            font-size: 14px;
-            box-shadow: none;
-            padding: 10px 40px;
-            border-radius: 5px;
-            font-weight: normal;
-            border: 1px solid #e5e5e5;
-            background-color: #004680;
-        }
-        button:hover:enabled {
-            cursor: pointer;
-        }
+      width: 100%;
     }
+  }
 }
-@-webkit-keyframes fadeIn {
-    from { opacity: 0; }
-    to { opacity: 1; }
+@media screen and (min-width: 425px) and (max-width: 768px) {
+  #signup {
+    .row {
+      .googleButton {
+      }
+      .input-name {
+      }
+      input {
+      }
+      .button {
+      }
+    }
+  }
 }
-
-@keyframes fadeIn {
-    from { opacity: 0; }
-    to { opacity: 1; }
+@media screen and (max-width: 425px) {
+  #complete {
+    .row {
+      input {
+        width: 250px;
+        min-width: unset;
+        max-width: unset;
+        box-sizing: border-box;
+      }
+      .button {
+        width: 250px;
+        min-width: unset;
+        max-width: unset;
+      }
+    }
+  }
 }
 </style>
--- src/pages/completeProfile.pages.vue
+++ src/pages/completeProfile.pages.vue
@@ -1,12 +1,17 @@
 <template>
 <div id="completeProfile">
-    <container display="flex">
-        <div class="content">
-            <h3 class="logo">Kaynak Yönetim Sistemi</h3>
-            <h5 class="welcome">Profil Bilgilerini Tamamla</h5>
-            <complete-profile-component></complete-profile-component>
-        </div>
-    </container>
+  <container display="flex">
+    <div class="wrapper">
+      <div class="form">
+        <h3 class="welcome">Welcome to Venue</h3>
+        <h5 class="message">Complete your profile</h5>
+        <complete-profile-component></complete-profile-component>
+      </div>
+      <div class="illustration">
+        <img class="login-illustration" src="../assets/illustrations/login.jpg" alt="">
+      </div>
+    </div>
+  </container>
 </div>
 </template>
 
@@ -18,66 +23,114 @@ import completeProfileComponent from '../components/complete.completeProfile'
 import { mapGetters } from 'vuex'
 import { usersCollection } from '../firebase/index'
 export default {
-    name: "completeProfile.pages",
-    components: { container, completeProfileComponent },
-    store,
-    router,
-    data: function () {
-        return {
-            name: '',
-            surname: '',
-            mobile: '',
-            email: ''
-        }
-    },
-    computed: {
-        ...mapGetters([
-            'getUser'
-        ])
-    },
-    created () {
-        // if exists
-        usersCollection.doc(this.getUser.uid).get()
-            .then(doc => {
-                if (doc.exists) {
-                    console.log("Document data:", doc.data())
-                    this.$store.dispatch('setUserInfo', doc.data())
-                    router.push("/")
-                } else {
-                    // doc.data() will be undefined in this case
-                    console.log("No such document!")
-                }
-            }).catch(function(error) {
-                console.log("Error getting document:", error)
-            })
+  name: "completeProfile.pages",
+  components: { container, completeProfileComponent },
+  store,
+  router,
+  data: function () {
+    return {
+      name: '',
+      surname: '',
+      mobile: '',
+      email: ''
     }
+  },
+  computed: {
+    ...mapGetters([
+      'getUser'
+    ])
+  },
+  created () {
+    // if exists
+    usersCollection.doc(this.getUser.uid).get()
+      .then(doc => {
+        if (doc.exists) {
+          console.log("Document data:", doc.data())
+          this.$store.dispatch('setUserInfo', doc.data())
+          router.push("/")
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!")
+        }
+      }).catch(function(error) {
+        console.log("Error getting document:", error)
+      })
+  },
+  mounted() {
+    window.addEventListener('resize', this.resize)
+    this.resize()
+  },
+  methods: {
+    resize: function () {
+      let h = document.documentElement.clientHeight;
+      let el = document.getElementsByClassName('wrapper')[0]
+      if (h > (el.offsetHeight + (document.getElementById('header').offsetHeight) + (document.getElementById('footer').offsetHeight))) {
+        let newHeight = h - (document.getElementById('header').offsetHeight) - (document.getElementById('footer').offsetHeight)
+        el.style.height = newHeight + 'px'
+      } else {
+        el.style.height = ''
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-#completeProfile {
-    display: flex;
-    height: calc(100vh - 96px);
-    align-items: center;
-    justify-content: center;
-}
 .container {
-    .content {
-        padding: 50px 0;
-        transition: opacity 1s ease-out;
+  .wrapper {
+    width: 100%;
+    display: flex;
+    padding: 50px 0;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  .wrapper {
+    .form {
     }
-    .content {
-        .logo {
-            font-size: 24px;
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .welcome {
-            font-size: 18px;
-            text-align: center;
-            margin-bottom: 30px;
-            font-weight: normal;
-        }
+    .form {
+      .welcome {
+        color: #000000;
+        font-size: 24px;
+        text-align: left;
+        font-weight: 700;
+        margin-bottom: 16px;
+        font-family: Roboto, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;
+      }
+      .message {
+        color: #707070;
+        font-size: 18px;
+        text-align: left;
+        font-weight: normal;
+        margin-bottom: 30px;
+        font-family: Roboto, Helvetica Neue Light, Helvetica Neue, Helvetica, Arial, Lucida Grande, sans-serif;
+      }
     }
+    .illustration {
+      overflow: hidden;
+      max-width: 400px;
+    }
+    .illustration {
+      .login-illustration {
+        width: 100%;
+      }
+    }
+  }
+}
+@media screen and (max-width: 768px) {
+  .container {
+    .wrapper {
+      display: flex;
+      padding: 24px 0;
+      align-items: center;
+      justify-content: center;
+    }
+    .wrapper {
+      .forms {
+      }
+      .illustration {
+        display: none;
+      }
+    }
+  }
 }
 </style>
