2daa8602a1fa603992011ad15cf7d495ee421079
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2020-12-26 03:23:28 +0100

    New component - New activity modal in Source reservation

---
+++ src/components/modal.createActivity.sourceReservation.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="modal">
+  <div class="modal">
+    hi
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'modal.createActivity.sourceReservation'
+}
+</script>
+
+<style lang="scss" scoped>
+#modal {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, .72);
+}
+#modal {
+  .modal {
+    padding: 20px;
+    color: #000000;
+    min-width: 300px;
+    max-width: 500px;
+    border-radius: 8px;
+    background-color: #ffffff;
+  }
+}
+</style>
