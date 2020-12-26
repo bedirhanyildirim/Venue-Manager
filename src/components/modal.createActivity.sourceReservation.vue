9040f2fe08366e334680c8378cba1a3dd2b42643
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2020-12-26 04:07:57 +0100

    Development - New activity modal click-away (toggle)

--- src/components/modal.createActivity.sourceReservation.vue
+++ src/components/modal.createActivity.sourceReservation.vue
@@ -1,14 +1,25 @@
 <template>
 <div id="modal">
-  <div class="modal">
+  <div class="modal" v-on-clickaway="away">
     hi
   </div>
 </div>
 </template>
 
 <script>
+import { mixin as clickaway } from 'vue-clickaway'
+
 export default {
-  name: 'modal.createActivity.sourceReservation'
+  name: 'modal.createActivity.sourceReservation',
+  mixins: [ clickaway ],
+  methods: {
+    away: function () {
+      this.$emit('away', false)
+    }
+  },
+  beforeMount() {
+    document.body.style.position = 'fixed'
+  }
 }
 </script>
 
@@ -16,10 +27,11 @@ export default {
 #modal {
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1000;
   display: flex;
-  position: absolute;
+  position: fixed;
   align-items: center;
   justify-content: center;
   background-color: rgba(0, 0, 0, .72);
@@ -28,9 +40,10 @@ export default {
   .modal {
     padding: 20px;
     color: #000000;
+    width: 500px;
     min-width: 300px;
-    max-width: 500px;
     border-radius: 8px;
+    position: relative;
     background-color: #ffffff;
   }
 }
