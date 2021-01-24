06a4743deac3a61d1ffb571c78f286f50d5b5699
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2021-01-24 03:53:27 +0100

    New component - My Sources

---
+++ src/components/mySources.home.vue
@@ -0,0 +1,173 @@
+<template>
+  <div id="mySources">
+    <h2 class="title">My Sources</h2>
+    <p class="sub-title">Manage your sources of your company</p>
+    <div class="list">
+      <div class="el" v-for="el in sources" @click="goToSource(el.id)">
+        <div class="image">
+          <img src=".././assets/images/source-img-16x9.jpg" alt="source" />
+        </div>
+        <div class="title">
+          <h5>{{ el.data.name }}</h5>
+        </div>
+        <div class="desc">
+          <h6>{{ el.data.description }}</h6>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import store from '@/store'
+import router from '@/router'
+import { mapGetters } from 'vuex'
+import {sourcesCollection} from '@/firebase'
+
+export default {
+  name: 'mySources.home.vue',
+  store,
+  router,
+  data: function () {
+    return {
+      sources: [],
+      noSource: false
+    }
+  },
+  created() {
+    sourcesCollection.where("company.owner.uid", "==", this.getUserInfo.uid).get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        console.log('No matching documents.')
+        this.noSource = true
+        return
+      }
+      snapshot.forEach(doc => {
+        this.sources.push({id: doc.id, data: doc.data()})
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      console.log(err.code)
+    })
+  },
+  computed: {
+    ...mapGetters([
+      'getUserInfo'
+    ])
+  },
+  methods: {
+    goToSource: function (sourceId) {
+      router.push('/my-source/'+sourceId)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#mySources {
+  width: 100%;
+  display: flex;
+  align-items: baseline;
+  justify-content: left;
+  flex-direction: column;
+}
+#mySources {
+  .title {
+    width: 100%;
+    text-align: left;
+    margin-bottom: 12px;
+    font-size: 24px;
+  }
+  .sub-title {
+    width: 100%;
+    text-align: left;
+    font-weight: 300;
+    font-size: 16px;
+  }
+  .list {
+    width: 100%;
+    display: flex;
+    overflow: hidden;
+    margin-top: 24px;
+    flex-direction: row;
+    align-items: baseline;
+    justify-content: flex-start;
+  }
+  .list {
+    .el {
+      width: 180px;
+      overflow: hidden;
+      margin-right: 24px;
+    }
+    .el {
+      .image {
+        width: fit-content;
+        height: fit-content;
+      }
+      .image {
+        img {
+          width: 100%;
+          display: block;
+          cursor: pointer;
+          border-radius: 8px;
+          transition: opacity 100ms ease;
+        }
+        img:hover {
+          opacity: .8;
+        }
+      }
+      .title {
+        width: 100%;
+        margin-top: 16px;
+        text-wrap: normal;
+        margin-bottom: 0px;
+      }
+      .title {
+        h5 {
+          color: #000000;
+          font-size: 16px;
+          overflow: hidden;
+          text-align: left;
+          cursor: pointer;
+          max-height: 17px;
+          line-height: 16px;
+          font-weight: normal;
+          display: inline-block;
+          text-overflow: ellipsis;
+          transition: all 100ms ease;
+        }
+        h5::after {
+          content: '';
+          width: 0px;
+          height: 1px;
+          display: block;
+          background: black;
+          transition: 300ms;
+        }
+        h5:hover::after {
+          width: 100%;
+        }
+      }
+      .desc {
+        width: 100%;
+        padding: 4px;
+      }
+      .desc {
+        h6 {
+          width: 100%;
+          color: #303030;
+          font-size: 12px;
+          overflow: hidden;
+          text-align: left;
+          max-height: 50px;
+          line-height: 18px;
+          font-weight: lighter;
+          text-overflow: ellipsis;
+        }
+      }
+    }
+    .el:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
